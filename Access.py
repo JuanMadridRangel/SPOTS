49980421e5b438a722e0b2683d9a692a78eb5a06
JuanMadridRangel <jmadrid@dts-freight.com>
2025-06-12 07:23:15 -0500

    Delete credentials

--- Access.py
+++ Access.py
@@ -1,17 +1,5 @@
 
 # -----------------------------
-# Google Maps API Credentials
-# -----------------------------
-API_KEY = "AIzaSyDWnxUDq9qjCXTQt38OLi6a2l1svGt-owM"
-
-# -----------------------------
-# DAT API Credentials
-# -----------------------------
-ORG_USERNAME = "apietrangelo@dts-freight.com"
-ORG_PASSWORD = "Anpi@146514651465"
-ACCOUNT_USERNAME = "jmadrid@dts-freight.com"
-
-# -----------------------------
 # DAT API URLs
 # -----------------------------
 org_url = "https://identity.api.dat.com/access/v1/token/organization"
@@ -32,7 +20,6 @@ base_url = "https://routes.googleapis.com/directions/v2:computeRoutes"
 # -----------------------------
 
 GS_CLIENT_ID = "directtrafficsolutions"     # client_id
-GS_CLIENT_SECRET = "7897a85a-7909-4a75-85b8-a92d48545992" # client_secret
 GS_AUTH_URL = "https://api.greenscreens.ai/v1/auth/token"
 GS_PREDICT_URL = "https://api.greenscreens.ai/v3/prediction/rates"
 
