0dd00b66e7fc0260dd91b0460e364b801a88668f
JuanMadridRangel <jmadrid@dts-freight.com>
2025-08-21 08:23:05 -0500

    Update Access.py

--- Access.py
+++ Access.py
@@ -8,7 +8,7 @@ user_url = "https://identity.api.dat.com/access/v1/token/user"
 #DAT RATE, MILES, MCI
 url_spot = "https://analytics.api.dat.com/linehaulrates/v1/lookups"
 url_forecast = "https://analytics.api.dat.com/linehaulrates/v1/forecasts/spot"
-url_MCI = "https://analytics.api.dat.com/marketconditions/v1/marketConditionsForecasts"
+url_MCI = "https://analytics.api.dat.com/marketconditions/v1/marketConditionsIndexes"
 
 # -----------------------------
 # Google Maps API URL
@@ -22,5 +22,6 @@ base_url = "https://routes.googleapis.com/directions/v2:computeRoutes"
 GS_CLIENT_ID = "directtrafficsolutions"     # client_id
 GS_AUTH_URL = "https://api.greenscreens.ai/v1/auth/token"
 GS_PREDICT_URL = "https://api.greenscreens.ai/v3/prediction/rates"
+
 
 
